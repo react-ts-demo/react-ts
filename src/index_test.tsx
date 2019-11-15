4c3dfb63f3090484b7603bbcf38ab84909256986
houlong <houlong@houlongdeMacBook-Pro.local>
2019-11-15 14:55:21 +0800

    feat:0.1.1

--- src/index_test.tsx
+++ src/index_test.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+// import {MyComponent} from './PropsComponent'
+// import StatePropsComp from './state_propsComp'
+// import RfcComp from './rfcComp'
 import {Upload} from './components/Upload'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <>
+    {/* <MyComponent name='propsComponents'/> */}
     <App/>  
     <Upload/>
+    {/* <StatePropsComp />
+    <RfcComp name='rfctest'/> */}
   </>,
   document.getElementById('root') as HTMLElement
 );
